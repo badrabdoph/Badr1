7ee9ef57acfe369b6330510cbc68f2dffbb23b05
badrabdoph-netizen <badrabdoph@icloud.com>
2026-02-21 02:05:11 +0200

    Update Contact.tsx

--- client/src/pages/Contact.tsx
+++ client/src/pages/Contact.tsx
@@ -1746,10 +1746,10 @@ export default function Contact() {
                 <div className="md:hidden">
                   <div className="wheel-picker">
                     <WheelColumn
-                      label="اليوم"
-                      value={baseDay}
-                      options={dayOptions}
-                      onChange={(day) => updateDateParts({ day })}
+                      label="السنة"
+                      value={baseYear}
+                      options={yearOptions}
+                      onChange={(year) => updateDateParts({ year })}
                     />
                     <WheelColumn
                       label="الشهر"
@@ -1758,10 +1758,10 @@ export default function Contact() {
                       onChange={(month) => updateDateParts({ month })}
                     />
                     <WheelColumn
-                      label="السنة"
-                      value={baseYear}
-                      options={yearOptions}
-                      onChange={(year) => updateDateParts({ year })}
+                      label="اليوم"
+                      value={baseDay}
+                      options={dayOptions}
+                      onChange={(day) => updateDateParts({ day })}
                     />
                   </div>
                 </div>
